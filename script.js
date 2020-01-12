649c00e574b61a691c45f545df518b87d3850542
abdu0413 <50367097+abdu0413@users.noreply.github.com>
2020-01-12 11:19:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+var name = 'Yousuf';
 
+const message = "My name is yousef"
+console.log(message)
+
+const number = 4 
+result = number * 2
+result = result * 5
+result = result/number
+result = result - 7 
